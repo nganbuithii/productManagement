ea1dce25fa1b9169f552b24b57b0834843664a17
Ngân <nganbui.23112003@gmail.com>
2024-01-31 06:43:10 +0700

    đẩy file ảnh lên cloud

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -133,8 +133,6 @@ module.exports.create = async (req, res) => {
 
 // [patch] /admin/products/create
 module.exports.createPost = async (req, res) => {
-
-
   // lấy ra data -  khi submit thì mình sẽ nhận data thông qua req.body
   // vì luuw trong database là kiểu số nên ta cần parseInt
   req.body.price = parseInt(req.body.price)
@@ -152,9 +150,9 @@ module.exports.createPost = async (req, res) => {
     req.body.position = parseInt(req.body.position)
   }
   //console.log(req.body)
-  if(req.file){
-      req.body.thumbnail = `/uploads/${req.file.filename}`
-  }
+  // if(req.file){
+  //     req.body.thumbnail = `/uploads/${req.file.filename}`
+  // }
 
   // code lưu sản phẩm xuống database
   const newproduct = new product(req.body)
--- routes/admin/product.router.js
+++ routes/admin/product.router.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const multer  = require('multer')
-const storageMulter = require("../../helpers/storageMulter")
-const upload = multer({storage: storageMulter()})
+//const storageMulter = require("../../helpers/storageMulter")
+const upload = multer()
 const router = express.Router();
 const validates = require("../../validates/admin/products.validate.js")
 
 const controller = require("../../controllers/admin/product.controller");
 const { validate } = require("../../models/product.model");
+
+const uploadCloud = require ("../../middlewares/admin/uploadCloud.middlewares.js")
 
 router.get("/", controller.index);
 
@@ -18,11 +20,11 @@ router.delete("/delete/:id", controller.deleteItem);
 
 router.get("/create", controller.create);// lấy phương thức get để ra giao diện
 
-router.post("/create", upload.single('thumbnail'),validates.createPost, controller.createPost);// khi submit form tạo mới - phương thức post
+router.post("/create", upload.single('thumbnail'),uploadCloud.upload,validates.createPost, controller.createPost);// khi submit form tạo mới - phương thức post
 
 
 router.get("/edit/:id",controller.edit)// để lấy ra giao diện
-router.patch("/edit/:id", upload.single('thumbnail'),validates.createPost, controller.editPatch);// khi submit thì cập nhật trong database
+router.patch("/edit/:id", uploadCloud.upload,validates.createPost, controller.editPatch);// khi submit thì cập nhật trong database
 
 router.get("/detail/:id",controller.detail)
 
