fdc3935abce355ac95a52ba01d0261820c587058
Ngân <nganbui.23112003@gmail.com>
2024-02-24 11:25:58 +0700

    hiển thị sản phẩm mới

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -16,8 +16,15 @@ module.exports.index = async(req, res) => {
     // tính giá tiền sau giảm
     const newProduct =productHelper.priceNewProducts(productFeatured)
     
+    //- lấy ra sản phẩm mới nhất, sắp xếp vị trí giảm dần
+    const productNew = await Product.find({
+        deleted:false,
+        status:"active",
+    }).sort({position: "desc"}).limit(6)
+    const newProductNews = productHelper.priceNewProducts(productNew)
     res.render("client/pages/home/index.pug",{
         pageTitle:"Trang chủ",
-        productFeatured:newProduct
+        productFeatured:newProduct,
+        productsNew : newProductNews
     })
 }
